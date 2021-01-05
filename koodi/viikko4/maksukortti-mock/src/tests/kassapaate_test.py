20221685479d7c87389be114a1a7c519bbc271ae
Matti Luukkainen <mluukkai@iki.fi>
2021-01-05 18:24:38 +0200

    Update kassapaate_test.py

--- koodi/viikko4/maksukortti-mock/src/tests/kassapaate_test.py
+++ koodi/viikko4/maksukortti-mock/src/tests/kassapaate_test.py
@@ -9,13 +9,17 @@ class TestKassapaate(unittest.TestCase):
         self.kassa = Kassapaate()
 
     def test_kortilta_velotetaan_hinta_jos_rahaa_on(self):
-        maksukortti_mock = Mock(wraps=Maksukortti(10))
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 10
+        
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_called_with(HINTA)
 
     def test_kortilta_ei_veloteta_jos_raha_ei_riita(self):
-        maksukortti_mock = Mock(wraps=Maksukortti(4))
+        maksukortti_mock = Mock()
+        maksukortti_mock.saldo = 4
+        
         self.kassa.osta_lounas(maksukortti_mock)
 
         maksukortti_mock.osta.assert_not_called()

35bdf632cf9eb754eb36719f33f09f63b0170caf
Matti Luukkainen <mluukkai@iki.fi>
2021-01-06 15:21:57 +0200

    Delete has_at_least.py

--- koodi/viikko6/query-language/src/matchers/has_at_least.py
+++
@@ -1,9 +0,0 @@
-class HasAtLeast:
-    def __init__(self, value, attr):
-        self._value = value
-        self._attr = attr
-
-    def matches(self, player):
-        player_value = getattr(player, self._attr)
-
-        return player_value >= self._value
